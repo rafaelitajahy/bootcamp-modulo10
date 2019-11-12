eeac878a5c8c59ebe5ae7aeddbc5c5ea9cf49522
rafaelitajahy <rafaelitajahy@gmail.com>
2019-11-12 11:44:34 -0300

    Finalizando app

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3337',
+  baseURL: 'http://localhost:3337', //conexão USB utilizar IP
 });
 
 export default api;
